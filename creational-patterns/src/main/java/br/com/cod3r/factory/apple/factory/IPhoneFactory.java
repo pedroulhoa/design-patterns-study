915f0a53eacae451476e036b492acd7d6c5e8956
Pedro Ulhoa <pedroulhoa@live.com>
2021-07-28 20:03:42 -0300

    init: start project / Factory Method

---
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -0,0 +1,34 @@
+package br.com.cod3r.factory.apple.factory;
+
+import br.com.cod3r.factory.apple.model.*;
+
+public abstract class IPhoneFactory {
+
+    public static IPhone orderIPhone(String generation, String level) {
+        IPhone device = null;
+
+        if ("X".equals(generation)) {
+            if ("standard".equals(level)) {
+                device = new IPhoneX();
+            } else if ("highEnd".equals(level)) {
+                device = new IPhoneXSMax();
+            }
+        } else if ("11".equals(generation)) {
+            if ("standard".equals(level)) {
+                device = new IPhone11();
+            } else if ("highEnd".equals(level)) {
+                device = new IPhone11Pro();
+            }
+        }
+
+        if (device != null) {
+            device.getHardware();
+            device.assemble();
+            device.certificates();
+            device.pack();
+        }
+
+        return device;
+    }
+
+}
