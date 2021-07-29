78f448a053dddef7fbab1942c3772fc6dda653ed
Pedro Ulhoa <pedroulhoa@live.com>
2021-07-29 15:21:33 -0300

    feat: Factory Method example DB

--- creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,23 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.db.OracleDB;
+import br.com.cod3r.factory.app.dbadapter.db.PostgresDB;
+
 public class Client {
 
 	public static void main(String[] args) {
+
+		DB oracleDB = new OracleDB();
+		DB postgresDB = new PostgresDB();
+
+		// Oracle
+		oracleDB.query("SELECT * FROM users");
+		oracleDB.update("INSERT INTO...");
+
+		//Postgres
+		postgresDB.query("SELECT * FROM users");
+		postgresDB.update("INSERT INTO...");
 
 	}
 }
