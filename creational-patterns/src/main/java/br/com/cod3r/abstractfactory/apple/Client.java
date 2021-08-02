dad6149591530761da814ac7f0a964898c2ed27d
Pedro Ulhoa <pedroulhoa@live.com>
2021-08-02 20:06:39 -0300

    feat: Abstract Factory example iPhone

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,31 @@
 package br.com.cod3r.abstractfactory.apple;
 
-public class Client {
-	
-	public static void main(String[] args) {
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.USRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
 
-	}
+public class Client {
+
+    public static void main(String[] args) {
+
+        // Rules
+        CountryRulesAbstractFactory brazilRules = new BrazilianRulesAbstractFactory();
+        CountryRulesAbstractFactory USRules = new USRulesAbstractFactory();
+
+        // Iphone Factory
+        IPhoneFactory genXFactory = new IPhoneXFactory(brazilRules);
+        IPhoneFactory gen11Factory = new IPhone11Factory(USRules);
+
+        // Ordering Iphone's
+        IPhone iPhoneX = genXFactory.orderIPhone("standard");
+        System.out.println(iPhoneX);
+
+        IPhone iPhone11 = gen11Factory.orderIPhone("highEnd");
+        System.out.println(iPhone11);
+
+    }
 }
--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/model/iphone/IPhone.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/model/iphone/IPhone.java
@@ -3,23 +3,25 @@ package br.com.cod3r.abstractfactory.apple.model.iphone;
 import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
 
 public abstract class IPhone {
-	CountryRulesAbstractFactory rules;
-	
-	public IPhone(CountryRulesAbstractFactory rules) {
-		this.rules = rules;
-	}
+    CountryRulesAbstractFactory rules;
 
-	public abstract void getHardware();
-	
-	public void assemble() {
-		System.out.println("Assembling all the hardwares");
-	}
+    public IPhone(CountryRulesAbstractFactory rules) {
+        this.rules = rules;
+    }
 
-	public void certificates() {
-		System.out.println("Testing all the certificates");
-	}
+    public abstract void getHardware();
 
-	public void pack() {
-		System.out.println("Packing the device");
-	}
+    public void assemble() {
+        System.out.println("Assembling all the hardwares");
+    }
+
+    public void certificates() {
+        System.out.println("Testing all the certificates");
+        System.out.println(rules.getCertificates().applyCertification());
+    }
+
+    public void pack() {
+        System.out.println("Packing the device");
+        System.out.println(rules.getPacking().pack());
+    }
 }
