62f6b9685c799267837d859ca8a217478aa01ebd
Pedro Ulhoa <pedroulhoa@live.com>
2021-09-21 07:55:31 -0300

    fix: prototype pattern example 1

--- creational-patterns/src/main/java/br/com/patterns/prototype/animation/Client.java
+++ creational-patterns/src/main/java/br/com/patterns/prototype/animation/Client.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.patterns.prototype.animation.model.Person;
+import br.com.patterns.prototype.animation.model.PersonSamples;
 
 public class Client {
 	private static List<Person> frames = new ArrayList<Person>();
@@ -25,8 +26,41 @@ public class Client {
 		System.out.println("* ");
 		System.out.print("**********************");
 	}
-	
-	public static void main(String[] args) {
-		
+
+	public static Person addLeft(Person p) throws CloneNotSupportedException {
+		Person person = (Person) p.clone();
+		person.left();
+		frames.add(person);
+		return person;
 	}
+
+	public static Person addRight(Person p) throws CloneNotSupportedException {
+		Person person = (Person) p.clone();
+		person.right();
+		frames.add(person);
+		return person;
+	}
+	
+	public static void main(String[] args) throws CloneNotSupportedException, InterruptedException {
+		PersonSamples samples = new PersonSamples();
+
+//		Person person = samples.get("stickMan");
+		Person person = samples.get("fatMan");
+		frames.add(person);
+		person = addLeft(person);
+		person = addLeft(person);
+		person = addLeft(person);
+		person = addRight(person);
+		person = addRight(person);
+		person = addRight(person);
+		person = addRight(person);
+		person = addRight(person);
+		person = addRight(person);
+		person = addRight(person);
+		person = addLeft(person);
+		person = addLeft(person);
+		person = addLeft(person);
+		animate();
+	}
+
 }
