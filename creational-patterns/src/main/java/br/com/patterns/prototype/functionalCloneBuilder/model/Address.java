2857b1e914b799ce87e1a532fa5fea2c8129adfb
Pedro Ulhoa <pedroulhoa@live.com>
2021-09-21 15:40:16 -0300

    fix: prototype pattern example 3

--- creational-patterns/src/main/java/br/com/patterns/prototype/functionalCloneBuilder/model/Address.java
+++ creational-patterns/src/main/java/br/com/patterns/prototype/functionalCloneBuilder/model/Address.java
@@ -1,25 +1,54 @@
 package br.com.patterns.prototype.functionalCloneBuilder.model;
 
-public class Address {
-	private String street;
-	private Integer number;
-	
-	public Address(String street, Integer number) {
-		super();
-		this.street = street;
-		this.number = number;
-	}
+public class Address implements Cloneable {
+    private String street;
+    private Integer number;
 
-	public String getStreet() {
-		return street;
-	}
+    public Address(String street, Integer number) {
+        super();
+        this.street = street;
+        this.number = number;
+    }
 
-	public Integer getNumber() {
-		return number;
-	}
+    public String getStreet() {
+        return street;
+    }
 
-	@Override
-	public String toString() {
-		return "Address [street=" + street + ", number=" + number + "]";
-	}
+    public Integer getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Address [street=" + street + ", number=" + number + "]";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static class Builder {
+        private String street;
+        private Integer number;
+
+        private Builder(String street, Integer number) {
+            this.street = street;
+            this.number = number;
+        }
+
+        public Builder withStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public Builder withNumber(Integer number) {
+            this.number = number;
+            return this;
+        }
+
+        public Address now() {
+            return new Address(street, number);
+        }
+    }
 }
--- creational-patterns/src/main/java/br/com/patterns/prototype/functionalCloneBuilder/model/User.java
+++ creational-patterns/src/main/java/br/com/patterns/prototype/functionalCloneBuilder/model/User.java
@@ -1,31 +1,73 @@
 package br.com.patterns.prototype.functionalCloneBuilder.model;
 
-public class User {
-	private String name;
-	private Integer age;
-	private Address address;
-	
-	public User(String name, Integer age, Address address) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.address = address;
-	}
-	
-	public String getName() {
-		return name;
-	}
+public class User implements Cloneable {
+    private String name;
+    private Integer age;
+    private Address address;
 
-	public Integer getAge() {
-		return age;
-	}
+    public User(String name, Integer age, Address address) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
 
-	public Address getAddress() {
-		return address;
-	}
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
-	}
+    public Integer getAge() {
+        return age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
+    }
+
+    @Override
+    protected User clone() throws CloneNotSupportedException {
+        Address cloneAddress = (Address) address.clone();
+        return new User(name, age, cloneAddress);
+    }
+
+    public Builder cloneBuilder() {
+        return new Builder(name, age, address);
+    }
+
+    public static class Builder {
+        private String name;
+        private Integer age;
+        private Address address;
+
+        private Builder(String name, Integer age, Address address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withAge(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder withAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public User now() {
+            return new User(name, age, address);
+        }
+
+    }
 }
