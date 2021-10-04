f74bdc68eea103919d8bf968cc1d255bee99dcc3
Pedro Ulhoa <pedroulhoa@live.com>
2021-10-04 11:52:18 -0300

    new: estructural patterns / decorator example

--- estructural-patterns/src/main/java/br/com/patterns/decorator/coffeeShop/Client.java
+++ estructural-patterns/src/main/java/br/com/patterns/decorator/coffeeShop/Client.java
@@ -3,9 +3,11 @@ package br.com.patterns.decorator.coffeeShop;
 import br.com.patterns.decorator.coffeeShop.model.Drink;
 import br.com.patterns.decorator.coffeeShop.model.Expresso;
 import br.com.patterns.decorator.coffeeShop.model.Tea;
+import br.com.patterns.decorator.coffeeShop.model.decorators.DoubleDrink;
+import br.com.patterns.decorator.coffeeShop.model.decorators.Milk;
 
 public class Client {
-	
+
 	public static void order(String name, Drink drink) {
 		System.out.println("Ordering a " + name);
 		drink.serve();
@@ -16,6 +18,8 @@ public class Client {
 	public static void main(String[] args) {
 		order("Expresso", new Expresso());
 		order("Tea", new Tea());
-		
+		order("Lungo", new DoubleDrink(new Expresso()));
+		order("Cafe Au Lait", new Milk(new Expresso()));
+		order("English Tea", new Milk(new Tea()));
 	}
 }
--- estructural-patterns/src/main/java/br/com/patterns/decorator/coffeeShop/model/decorators/DrinkDecorator.java
+++ estructural-patterns/src/main/java/br/com/patterns/decorator/coffeeShop/model/decorators/DrinkDecorator.java
@@ -3,5 +3,10 @@ package br.com.patterns.decorator.coffeeShop.model.decorators;
 import br.com.patterns.decorator.coffeeShop.model.Drink;
 
 public abstract class DrinkDecorator implements Drink {
-	
+
+    protected Drink drink;
+
+    public DrinkDecorator(Drink drink) {
+        this.drink = drink;
+    }
 }
