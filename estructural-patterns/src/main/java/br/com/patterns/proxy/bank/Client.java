b4318c05b064536e02928da02bfbe8d7b2737066
Pedro Ulhoa <pedroulhoa@live.com>
2021-10-23 21:41:14 -0300

    new: estructural patterns / proxy example

--- estructural-patterns/src/main/java/br/com/patterns/proxy/bank/Client.java
+++ estructural-patterns/src/main/java/br/com/patterns/proxy/bank/Client.java
@@ -1,5 +1,6 @@
 package br.com.patterns.proxy.bank;
 
+import br.com.patterns.proxy.bank.services.ATM;
 import br.com.patterns.proxy.bank.services.Bank;
 import br.com.patterns.proxy.bank.services.BankOperations;
 
@@ -14,8 +15,15 @@ public class Client {
 		bank.changePassword(123L, "1111", "4321");
 		bank.changePassword(123L, "1234", "4321");
 		bank.withdraw(123L, "4321", 700L);
-		
+
 		System.out.println("------------- ATM -------------");
-		
+		BankOperations atm = new ATM(bank);
+		atm.deposit(123L, 500L);
+		atm.withdraw(456L, "8888", 50L);
+		atm.withdraw(456L, "9999", 50L);
+		atm.withdraw(123L, "1234", 2000L);
+		atm.changePassword(123L, "1111", "4321");
+		atm.changePassword(123L, "1234", "4321");
+		atm.withdraw(123L, "4321", 700L);
 	}
 }
--- estructural-patterns/src/main/java/br/com/patterns/proxy/bank/model/User.java
+++ estructural-patterns/src/main/java/br/com/patterns/proxy/bank/model/User.java
@@ -1,53 +1,53 @@
 package br.com.patterns.proxy.bank.model;
 
 public class User {
-	private String name;
-	private Long accountNumber;
-	private String password;
-	private Long balance;
-	
-	public User(String name, Long accountNumber, String password, Long cash) {
-		this.name = name;
-		this.accountNumber = accountNumber;
-		this.password = password;
-		this.balance = cash;
-	}
+    private String name;
+    private Long accountNumber;
+    private String password;
+    private Long balance;
 
-	public String getName() {
-		return name;
-	}
+    public User(String name, Long accountNumber, String password, Long cash) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.password = password;
+        this.balance = cash;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Long getAccountNumber() {
-		return accountNumber;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setAccountNumber(Long accountNumber) {
-		this.accountNumber = accountNumber;
-	}
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public Long getBalance() {
-		return balance;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setBalance(Long cash) {
-		this.balance = cash;
-	}
+    public Long getBalance() {
+        return balance;
+    }
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", accountNumber=" + accountNumber + ", password=" + password + ", balance=" + balance
-				+ "]";
-	}
+    public void setBalance(Long cash) {
+        this.balance = cash;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", accountNumber=" + accountNumber + ", password=" + password + ", balance=" + balance
+                + "]";
+    }
 }
